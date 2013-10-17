f1845e8ddd97ca431f4265120c8d289c7eb38eeb
Scott Kirkland <srkirkland@gmail.com>
2013-10-17 10:44:46 -0700

    start of copy script

--- AzureBlobBackup/Program.cs
+++ AzureBlobBackup/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace AzureBlobBackup
 {
@@ -14,7 +17,17 @@ namespace AzureBlobBackup
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            Console.WriteLine("Getting Azure Storage Configuration");
 
+            var storageAccount =
+                CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            var blobClient = storageAccount.CreateCloudBlobClient();
+
+            var container = blobClient.GetContainerReference(CloudConfigurationManager.GetSetting("FileContainer"));
+            
+            
+            
         }
     }
 }
